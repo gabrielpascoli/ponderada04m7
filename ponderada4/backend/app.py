93a50abc4325d6e5fbfe16aac35044693cda7c51
gabrielpascoli <99223332+gabrielpascoli@users.noreply.github.com>
2023-10-02 22:46:25 -0300

    feat: tests

--- ponderada4/backend/app.py
+++ ponderada4/backend/app.py
@@ -71,5 +71,4 @@ def signup():
     
     return 'Cadastro realizado com sucesso!'
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+app.run(host='0.0.0.0', port=80, debug=True)
